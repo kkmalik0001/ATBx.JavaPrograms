a65cb21548966322b5eafeb7f426375d8e05dd57
kkmalik0001 <kkmalik0001@gmail.com>
2024-10-04 16:16:03 +0530

    keyword, identifier and data type

---
+++ src/sept/ex_09092024/Lab0010_keywords_and_identifier.java
@@ -0,0 +1,15 @@
+package sept.ex_09092024;
+
+public class Lab0010_keywords_and_identifier {
+    public static void main (String[] args){
+        // keyword and identifiers
+        // Keyword is nothing but reserved words
+        // these are simple english word which has a pre-defined meaning in java program.
+        // they are in orange in  color in Intellij
+        // example: package, public , class , static , void
+        // Main is not a keyword
+        // will be in lower case always
+        // compiler understand these keywords
+        // all keywords have a special meaning, you can't change its owm
+    }
+}
---
+++ src/sept/ex_09092024/Lab0011_variableANDdatatypes_Identifiers.java
@@ -0,0 +1,29 @@
+/*
+*
+* Variables and Data Types, Identifier
+*
+* */
+
+package sept.ex_09092024;
+
+public class Lab0011_variableANDdatatypes_Identifiers {
+    public static void main(String[] args) {
+
+        //Variables and Data types , Literal, Identifier
+
+        // Variable are used to store a value
+        /*
+        * Types of data ? which can be store
+        * 1. Integers can store-> whole number (-1,2,0...)
+        * 2. 3.14, 18.75,....
+        * 3. True or False
+        * 4. String ( "kapil") can store
+        * 5. Character ( 'c')
+        * 6. large number ( 9876543212132184321432984) can store
+        *
+        * */
+
+        boolean are_you_married = false ;    // boolean -> data type, are_you_married-> variable name , Data value-> true or false
+        System.out.println();
+    }
+}
